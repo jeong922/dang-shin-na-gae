9cdd5f807ac70fb8da6255437a7fee41bc78f276
jeong922 <hwajeong922@naver.com>
2026-07-13 13:43:26 +0900

    feat: FastAPI 개발 환경 세팅

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import pandas as pd
-import folium
+# import pandas as pd
+# import folium
 
-print("파이썬 환경 세팅 완료!")
-print(f"Pandas 버전: {pd.__version__}")
+# print("파이썬 환경 세팅 완료!")
+# print(f"Pandas 버전: {pd.__version__}")
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
